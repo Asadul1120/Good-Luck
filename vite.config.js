197744e05d6df7ba1ea6ac6415494b7f26a50f18
Asadul Islam <asadulcb45@gmail.com>
2026-02-18 23:42:29 +0600

    dpg rqu  work

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,15 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+// })
+
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -5,4 +17,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 3948   // এখানে যেই port চান দিন
+  }
 })
